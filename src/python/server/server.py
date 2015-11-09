a72e27139a62f8e57ae92165d725c3f454e54f60
HMT <HMT@HMT.local>
2015-11-10 03:05:15 +0900

    make_sound of server

--- src/python/server/server.py
+++ src/python/server/server.py
@@ -19,26 +19,28 @@ import urlparse
 import zipfile
 import time
 import socket
+import sys
+import os
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 from SocketServer import ThreadingMixIn
 
 
 
 class Handler(BaseHTTPRequestHandler):
-    '''
-    def __init__(self):
-        self.expression = "y=x^2"
+
+    #初期化用関数
+    def init(self):
         self.imageBool = False
         self.soundBool = False
-        self.range = [-5,5]
-    '''
+        self.range = ""
+        self.exp = ""
 
     def judge_param(self,param,value):
+        
+
         if(param == "expression"):
-            self.expression = value
-            '''
-            数式から画像、音声を生成する処理を行う
-            '''
+            self.exp = value
+
         if(param == "image"):
             if(value):
                 self.imageBool = True
@@ -46,8 +48,21 @@ class Handler(BaseHTTPRequestHandler):
         if(param == "sound"):
             if(value):
                 self.imageBool = True
+
         if(param == "range"):
             self.range = map(int,value.split(":"))
+            self.beg = self.range[0]
+            self.end = self.range[1]
+            print "range: " + str(self.beg) + "," + str(self.end)
+
+    def make_response(self):
+        print "exp: "+ str(self.exp) + "range: " + str(self.range)
+        if(self.exp and self.range):
+            print "make responce"
+            xPosArray, yPosArray = parseExpression.getCoordinate(self.exp, int(self.beg), int(self.end))
+            print "x = " + str(xPosArray)
+            print "y = " + str(yPosArray)
+            gensound.genSound(yPosArray)
 
     def read_file(self):
         message_parts=[""]
@@ -59,27 +74,30 @@ class Handler(BaseHTTPRequestHandler):
     def zip(self):
         zipFile = zipfile.ZipFile("./send.zip","w",zipfile.ZIP_DEFLATED)
         zipFile.write("./image.png")
-        zipFile.write("./sound.mp3")
+        zipFile.write("./output.mp3")
         zipFile.close()
 
     def do_GET(self):
         path = urlparse.urlparse(self.path)
         params = urlparse.parse_qsl(self.path)
+        self.init()
         for i,param in enumerate(params):
             print param
+            #expressionはクエリの"&""がつくので外して渡す。
             if(i == 0):
                 self.judge_param(param=str(param[0])[2:],value=param[1])
                     
             else:
                 self.judge_param(param=param[0],value=param[1])
-        
+        self.make_response()
+
         #responce
         #time.sleep(10)         #wait check
         self.send_response(200)
         self.end_headers()
         self.zip()
         self.read_file()
-        self.wfile.write(self.message)
+        #self.wfile.write(self.message)
         return 
 
 
@@ -88,6 +106,10 @@ class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     """Handle requests in a separate thread."""
 
 if __name__ == '__main__':
+    sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))) )
+    from make_sound import *
+    from numerical_formula import *
+
     myIP = socket.gethostbyname(socket.gethostname())
     print "myIP :" + myIP
     server = ThreadedHTTPServer((myIP, 8080), Handler)
